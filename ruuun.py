e6b049e17ed317a692f823cb92e548432049e769
xtinalucia <christina.lucia123@gmail.com>
2021-10-12 01:23:49 -0500

    second

--- ruuun.py
+++ ruuun.py
@@ -1,14 +1,4 @@
-from flask import Flask
+from app import app
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return 'Hi Hi'
-
-@app.route('/testlala')
-def test():
-    return 'test'
-
-if __name__ == '__main__':
+if __name__=='__main__':
     app.run()
